a663c9a3bac28978f052139f5b21647388e528c0
jskyzero <jskyzero@outlook.com>
2019-06-08 13:44:16 +0800

    make animation & controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Player : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public float x = 0;
+  public float speed = 0.01f;
+  private Animator animator;
+  private Rigidbody2D body;
+
+  // Start is called before the first frame update
+  void Start() {
+    animator = this.GetComponent<Animator>();
+    body = this.GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    x = Input.GetAxis("Horizontal");
+    animator.SetFloat("x", x);
+
+    var possiton = this.transform.position;
+    possiton.x += x * speed;
+    this.transform.position = possiton;
+    // body.AddForce(new Vector2(x * speed, 0));
+  }
 }
