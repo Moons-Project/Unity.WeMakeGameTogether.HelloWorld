76d1779843e8876ebb19c51065164f5ff6c103a1
jskyzero <jskyzero@outlook.com>
2019-06-08 16:53:50 +0800

    update resources

---
+++ Assets/Scripts/Transport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transport : MonoBehaviour {
+
+  public GameObject fly;
+  public string sceneName;
+
+  void OnTriggerEnter2D(Collider2D col) {
+    Debug.Log(col);
+    if (col.gameObject.tag.Equals("Player")) {
+      fly.SetActive(true);
+      fly.GetComponent<Animator>().Play("fly_fly");
+      col.gameObject.GetComponent<Animator>().Play("fly");
+      StartCoroutine(PlayerAttack());
+    }
+  }
+
+  IEnumerator PlayerAttack() {
+    yield return new WaitForSeconds(1.0f);
+    SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+  }
+}
